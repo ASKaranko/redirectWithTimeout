daaec56efc839c5cf032414e58b3903577b40926
Karanko Andrei <andr.karanko@gmail.com>
2021-09-25 16:37:26 +0300

    Minor changes in link

--- redirect.js
+++ redirect.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('btn');
-const domain = 'https://everinform.com/';
-const tail = '?lang=ro&currency=usd&source_id=18&source_site_id=&source_campaign_id=140718&source_teaser_id=';
+const link = 'https://everinform.com/';
 const urlHref = window.location.href;
 const folder = urlHref.replace(/^\S*\.\w{2,4}\//ig, '').split('?')[0].replace(/\/$/, '').replace(/\//g, '_');
 
@@ -9,5 +8,5 @@ btn.addEventListener('click', (event) => {
 });
 
 function redirectOnTimeout() {
-    location.href = domain + folder + tail;
+    location.href = link + folder;
 }
