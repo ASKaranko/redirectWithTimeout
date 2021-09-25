831df7fbb290d47f3774deb20ab6489e0d54ceb2
Karanko Andrei <andr.karanko@gmail.com>
2021-09-25 16:47:12 +0300

    Minor changes in link

--- redirect.js
+++ redirect.js
@@ -1,12 +1,12 @@
 const btn = document.getElementById('btn');
 const link = 'https://everinform.com/';
 const urlHref = window.location.href;
-const folder = urlHref.replace(/^\S*\.\w{2,4}\//ig, '').split('?')[0].replace(/\/$/, '').replace(/\//g, '_');
+const preland = urlHref.replace(/^\S*\.\w{2,4}\//ig, '').split('?')[0].replace(/\/$/, '').replace(/\//g, '_');
 
 btn.addEventListener('click', (event) => {
     setTimeout(redirectOnTimeout, 2000);
 });
 
 function redirectOnTimeout() {
-    location.href = link + folder;
+    location.href = link + preland;
 }
