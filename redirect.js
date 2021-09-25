0366366f1d34ac23ace8592ffbe5402201f9ad7e
Karanko Andrei <andr.karanko@gmail.com>
2021-09-25 15:21:13 +0300

    Added regular expression for tail folder url

--- redirect.js
+++ redirect.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+const btn = document.getElementById('btn');
+const domain = 'https://everinform.com/';
+const tail = '?lang=ro&currency=usd&source_id=18&source_site_id=&source_campaign_id=140718&source_teaser_id=';
+const urlHref = window.location.href;
+const folder = urlHref.replace(/^\S*\.\w{2,4}\//ig, '').split('?')[0].replace(/\/$/, '').replace(/\//g, '_');
 
-    const btn = document.getElementById('btn');
-    btn.addEventListener('click', (event) => {
-        setTimeout(redirectOnTimeout, 2000);
-    });
-
-    function redirectOnTimeout() {
-        location.href = "https://everinform.com/?lang=ro&currency=usd&source_id=18&source_site_id=&source_campaign_id=140718&source_teaser_id=";
-    }
-  
+btn.addEventListener('click', (event) => {
+    setTimeout(redirectOnTimeout, 2000);
 });
+
+function redirectOnTimeout() {
+    location.href = domain + folder + tail;
+}
