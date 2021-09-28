0d58a579eff5755641d67de28e637d535161d029
Karanko Andrei <andr.karanko@gmail.com>
2021-09-28 11:58:20 +0300

    all a tags in listener

--- redirect.js
+++ redirect.js
@@ -1,12 +1,13 @@
-const btn = document.getElementById('btn');
 const link = 'https://everinform.com/';
 const urlHref = window.location.href;
 const preland = urlHref.replace(/^\S*\.\w*\//, '').split('?')[0].replace(/\/$/, '').replace(/\//g, '_');
 
-btn.addEventListener('click', (event) => {
-    setTimeout(redirectOnTimeout, 2000);
+document.querySelector('body').addEventListener('click', (event) => {
+    if (event.target.tagName && event.target.tagName.toUpperCase() === 'A') {
+        setTimeout(redirectOnTimeout, 2000);
+    }
 });
 
 function redirectOnTimeout() {
-    location.href = link + 'preland=' +preland;
+    location.href = link + '?preland=' + preland;
 }
